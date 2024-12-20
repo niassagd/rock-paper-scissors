96751ae4ddb8f85e1ad80172225501772d97b95f
niassagd <emailgchang@gmail.com>
2024-12-20 18:33:33 -0500

    Create function getComputerChoice for computer to make choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+
+function getComputerChoice() {
+        let rock = 0;
+        let paper = 1;
+        let scissors = 2;
+        let numChoice = Math.floor(Math.random()*3);
+        console.log(numChoice);
+        if (numChoice === 2) {
+            compChoice = "rock";
+        } else if (numChoice === 1) {
+            compChoice = "paper";
+        } else {
+            compChoice = "scissors";
+            
+        }
+        console.log(compChoice);
+    }
