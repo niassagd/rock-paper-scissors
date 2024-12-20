5de2185bbb0d54520e15f0dbf644ab9538e64aa3
niassagd <emailgchang@gmail.com>
2024-12-20 18:59:04 -0500

    Create function getHumanChoice to prompt user to make choice

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 
+//Computer chooses rock, paper, or scissors string every time function is run
 function getComputerChoice() {
-        let rock = 0;
-        let paper = 1;
-        let scissors = 2;
-        let numChoice = Math.floor(Math.random()*3);
+    let rock = 0;
+    let paper = 1;
+    let scissors = 2;
+    let numChoice = Math.floor(Math.random()*3);
         console.log(numChoice);
-        if (numChoice === 2) {
-            compChoice = "rock";
-        } else if (numChoice === 1) {
-            compChoice = "paper";
-        } else {
-            compChoice = "scissors";
+    if (numChoice === 2) {
+        compChoice = "rock";
+    } else if (numChoice === 1) {
+        compChoice = "paper";
+    } else {
+        compChoice = "scissors";
             
-        }
-        console.log(compChoice);
     }
+    console.log(compChoice);
+}
+//Human choose rock, paper, or scissors
+function getHumanChoice() {
+    let humanChoiceMade = false;
+    let humanChoice = prompt("Enter one option: rock, paper, scissors", "");
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+        humanChoiceMade = true;
+        console.log(humanChoiceMade);
+        console.log(humanChoice);
+    } else {
+        alert("?? Please spell your choice correctly in lowercase and no spaces");
+        humanChoiceMade = false;
+        console.log(humanChoiceMade);
+        console.log(humanChoice);
+        getHumanChoice();
+    }
+    }
+    
